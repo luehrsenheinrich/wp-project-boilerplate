0ddcb832a80955c11cdc87956b8ddedbd5e33757
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-03-15 14:19:19 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,48 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 
+
+/**
+ * Gather the files to be bundled
+ */
+
+/**
+ * Theme frontend JavaScript files
+ *
+ * @type {string[]}
+ */
 const ThemeFrontendJs = glob.sync('./theme/src/js/*.js').reduce(function (obj, el) {
 	obj['js/' + path.parse(el).name + '.min'] = el;
 	obj['js/' + path.parse(el).name + '.bundle'] = el;
 	return obj;
 }, {});
 
+/**
+ * Theme frontend CSS files
+ *
+ * @type {string[]}
+ */
 const ThemeFrontendCSS = glob.sync('./theme/src/css/*.css').reduce(function (obj, el) {
 	obj['css/' + path.parse(el).name + '.min'] = el;
 	return obj;
 }, {});
 
+/**
+ * Theme frontend PHP files
+ *
+ * @type {string[]}
+ */
 const ThemePhp = glob.sync('./theme/**/*.php').reduce(function (obj, el) {
     obj['php/' + path.parse(el).name] = el;
     return obj;
 }, {});
 
+
+/**
+ * Our default webpack config
+ *
+ * @type {Object}
+ */
 const defaultConfig = {
 	mode: 'production',
 	optimization: {
