97e5034c49ec2a58a31619ebca27896f0df00fa7
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-03-17 13:28:25 +0100

    Refine the webpack workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,13 @@
+/**
+ * External dependencies
+ */
 const glob = require('glob');
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
-
-
-/**
- * Gather the files to be bundled
- */
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 /**
  * Theme frontend JavaScript files
@@ -32,7 +31,28 @@ const ThemeFrontendCSS = glob.sync('./theme/src/css/*.css').reduce(function (obj
 }, {});
 
 /**
- * Theme frontend PHP files
+ * Theme backend JavaScript files
+ *
+ * @type {string[]}
+ */
+const ThemeBackendJs = glob.sync('./theme/admin/src/js/*.js').reduce(function (obj, el) {
+    obj['js/' + path.parse(el).name + '.min'] = el;
+    obj['js/' + path.parse(el).name + '.bundle'] = el;
+    return obj;
+}, {});
+
+/**
+ * Theme backend CSS files
+ *
+ * @type {string[]}
+ */
+const ThemeBackendCSS = glob.sync('./theme/admin/src/css/*.css').reduce(function (obj, el) {
+    obj['css/' + path.parse(el).name + '.min'] = el;
+    return obj;
+}, {});
+
+/**
+ * Theme PHP files
  *
  * @type {string[]}
  */
@@ -41,6 +61,65 @@ const ThemePhp = glob.sync('./theme/**/*.php').reduce(function (obj, el) {
     return obj;
 }, {});
 
+/**
+ * Plugin backend JavaScript files
+ *
+ * @type {string[]}
+ */
+const PluginBackendJs = glob.sync('./plugin/admin/src/js/*.js').reduce(function (obj, el) {
+    obj['js/' + path.parse(el).name + '.min'] = el;
+    obj['js/' + path.parse(el).name + '.bundle'] = el;
+    return obj;
+}, {});
+
+/**
+ * Plugin backend CSS files
+ *
+ * @type {string[]}
+ */
+const PluginBackendCSS = glob.sync('./plugin/admin/src/css/*.css').reduce(function (obj, el) {
+    obj['css/' + path.parse(el).name + '.min'] = el;
+    return obj;
+}, {});
+
+/**
+ * The default JS loader.
+ * This is a very vanilla JS loader based on Babel preset-env.
+ *
+ * @type {Object}
+ */
+const defaultJsLoader = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['@babel/preset-env'],
+        },
+    },
+};
+
+/**
+ * The WordPress JS loader.
+ * The opinionated WordPress JS loader. We only use this within the WordPress admin.
+ *
+ * @type {Object}
+ */
+const wordpressJsLoader = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+        loader: 'babel-loader',
+        options: {
+            presets: ['@wordpress/babel-preset-default'],
+            plugins: [
+                '@wordpress/babel-plugin-import-jsx-pragma',
+                '@babel/plugin-transform-react-jsx',
+                'lodash',
+            ],
+        },
+    },
+};
 
 /**
  * Our default webpack config
@@ -57,7 +136,6 @@ const defaultConfig = {
 		],
 	},
     output: {
-        path: path.resolve(__dirname, 'theme/dist'),
         clean: true,
         filename: '[name].js',
     },
@@ -68,7 +146,9 @@ const defaultConfig = {
         new LiveReloadPlugin({
             useSourceHash: true,
         }),
-        new RemoveEmptyScriptsPlugin(),
+        new RemoveEmptyScriptsPlugin({
+            extensions: ['css', 'php'],
+        }),
         new MiniCssExtractPlugin({
             filename: "[name].css",
         }),
@@ -99,40 +179,103 @@ const defaultConfig = {
             },
             {
                 test: /\.php$/,
-                loader: 'raw-loader',
+                loader: 'null-loader',
             }
         ],
     },
 };
 
-module.exports = [
-    {
-        ...defaultConfig,
-        name: 'themeFrontend',
-        entry: {
-            ...ThemeFrontendJs,
-            ...ThemeFrontendCSS,
-            ...ThemePhp,
-        },
-        output: {
-            ...defaultConfig.output,
-            path: path.resolve(__dirname, 'theme/dist'),
-        },
-        module: {
-            rules: [
-                ...defaultConfig.module.rules,
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env'],
-                        },
-                    },
-                },
-
-            ]
-        }
+/**
+ * The webpack config to bundle CSS and JS for the Theme frontend.
+ *
+ * @type {Object}
+ */
+const themeFrontendWebpackOptions = {
+    ...defaultConfig,
+    name: 'themeFrontend',
+    entry: {
+        ...ThemeFrontendJs,
+        ...ThemeFrontendCSS,
+        ...ThemePhp,
+    },
+    output: {
+        ...defaultConfig.output,
+        path: path.resolve(__dirname, 'theme/dist'),
+    },
+    module: {
+        rules: [
+            ...defaultConfig.module.rules,
+            defaultJsLoader,
+        ]
     }
+};
+
+/**
+ * The webpack config to bundle CSS and JS for the Theme backend.
+ *
+ * @type {Object}
+ */
+const themeBackendWebpackOptions = {
+    ...defaultConfig,
+    name: 'themeBackend',
+    entry: {
+        ...ThemeBackendJs,
+        ...ThemeBackendCSS,
+    },
+    output: {
+        ...defaultConfig.output,
+        path: path.resolve(__dirname, 'theme/admin/dist'),
+    },
+    module: {
+        rules: [
+            ...defaultConfig.module.rules,
+            wordpressJsLoader,
+        ]
+    },
+    plugins: [
+        ...defaultConfig.plugins,
+        new DependencyExtractionWebpackPlugin({
+            outputFormat: 'json',
+            combineAssets: true,
+        })
+    ]
+};
+
+/**
+ * The webpack config to bundle CSS and JS for the Plugin backend.
+ *
+ * @type {Object}
+ */
+const pluginBackendWebpackOptions = {
+    ...defaultConfig,
+    name: 'pluginBackend',
+    entry: {
+        ...PluginBackendJs,
+        ...PluginBackendCSS,
+    },
+    output: {
+        ...defaultConfig.output,
+        path: path.resolve(__dirname, 'plugin/admin/dist'),
+    },
+    module: {
+        rules: [
+            ...defaultConfig.module.rules,
+            wordpressJsLoader,
+        ]
+    },
+    plugins: [
+        ...defaultConfig.plugins,
+        new DependencyExtractionWebpackPlugin({
+            outputFormat: 'json',
+            combineAssets: true,
+        })
+    ]
+};
+
+
+
+module.exports = [
+    themeFrontendWebpackOptions,
+    pluginBackendWebpackOptions,
+    themeBackendWebpackOptions,
 ]
