cc225226f439af1d6e9522d3b34b0f1a30219004
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-03-28 09:31:43 +0200

    Lint the theme/plugin updater

--- bin/updatePluginVersion.js
+++ bin/updatePluginVersion.js
@@ -4,40 +4,34 @@
  * contains the version number, and replacing it with the new version.
  */
 
+/**
+ * We do not want eslint to report console calls.
+ */
+/* eslint-disable no-console */
+
 const fs = require('fs');
 const pkg = require('../package.json');
 
 fs.readFile(`./plugin/${pkg.slug}p.php`, (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+	if (err) {
+		console.error(err);
+		return;
+	}
 
-    // Find the file doc comment.
-    const fileDocRegex = /<\?php\s(\/\*(?:[^*]|\n|(?:\*(?:[^\/]|\n)))*\*\/)\s/;
-    const versionRowRegex = /[\s?]\*[\s?]Version:[\s?]\d.\d.\d/;
+	// Find the file doc comment.
+	const versionRowRegex = /[\s?]\*[\s?]Version:[\s?]\d.\d.\d/;
 
-    const uppercaseSlug = pkg.slug.toUpperCase();
+	// Build the new version row.
+	const newVersionRow = ` * Version: ${pkg.version}`;
 
-    // Find the version constant.
-    const versionRegex = new RegExp("define\\( '" + uppercaseSlug + "P_VERSION', '(.*)' \\);");
+	// Replace the existing version row.
+	const newData = data.toString().replace(versionRowRegex, newVersionRow);
 
-    // Extract the existing file doc comment.
-    const fileDocComment = data.toString().match(fileDocRegex)[0];
-
-    // Build the new version row.
-    const newVersionRow = ` * Version: ${pkg.version}`;
-
-    // Replace the existing version row.
-    let newData = data.toString().replace(versionRowRegex, newVersionRow);
-
-    // Write the new content to the file.
-    fs.writeFile(`./plugin/${pkg.slug}p.php`, newData, (err) => {
-        console.log( `Plugin version in ${pkg.slug}.php updated.` );
-        if (err) {
-            console.error(err);
-            return;
-        }
-    });
+	// Write the new content to the file.
+	fs.writeFile(`./plugin/${pkg.slug}p.php`, newData, () => {
+		console.log(`Plugin version in ${pkg.slug}.php updated.`);
+		if (err) {
+			console.error(err);
+		}
+	});
 });
-
