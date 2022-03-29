9830effec360c7c96cc254d58dd1beb470f502c8
Christopher Kanitz <ckanitz@users.noreply.github.com>
2022-03-29 14:59:46 +0200

    add example pattern

---
+++ theme/inc/Block_Patterns/bp-example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The block pattern for the example pattern.
+ *
+ * @package lhpbpt
+ */
+
+?>
+
+<!-- wp:paragraph -->
+<p>A simple example pattern. If you can read this at prod call an admin.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/block-patterns/placeholder_000.png" alt="" class=""/></figure>
+<!-- /wp:image -->
