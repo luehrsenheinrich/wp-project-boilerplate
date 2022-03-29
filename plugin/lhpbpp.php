268886cd8025f03148adaff43b8273d70abd2e74
Luehrsen <gh-actions-Luehrsen@github.com>
2022-03-29 11:48:50 +0000

    Tagging 0.0.10

--- plugin/lhpbpp.php
+++ plugin/lhpbpp.php
@@ -9,7 +9,7 @@
  * Description: A base boilerplate for Luehrsen // Heinrich WordPress projects.
  * Author: Luehrsen // Heinrich
  * Author URI: https://www.luehrsen-heinrich.de
- * Version: 0.0.9
+ * Version: 0.0.10
  * Text Domain: lhpbpp
  * Domain Path: /languages
  */
