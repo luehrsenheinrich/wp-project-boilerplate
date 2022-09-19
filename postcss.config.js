6d976cff17b7615f65f44249403946e09327ff4d
Hendrik Luehrsen <hendrik@luehrsen.de>
2022-09-19 11:51:11 +0200

    Format js

--- postcss.config.js
+++ postcss.config.js
@@ -1,35 +1,31 @@
 const colorPaletteGenerator = require('./bin/colorPaletteGenerator');
 
 module.exports = {
-    plugins: [
-        [
-            '@luehrsenheinrich/postcss-wp-global-styles',
-            {
-                themeJson: './theme/theme.json',
-                colorPaletteGenerator,
-            }
-        ],
-        [
-            'postcss-import',
-        ],
-        [
-            'postcss-preset-env',
-            {
-                stage: 1,
-                features: {
-                    'custom-media-queries': {
-                        preserve: false,
-                        importFrom: './theme/src/css/vars/_media-queries.css',
-                    },
-                    'custom-properties': {
-                        preserve: true,
-                        importFrom: './theme/src/css/vars.css',
-                    },
-                },
-            }
-        ],
-        [
-            'cssnano'
-        ]
-    ]
-}
+	plugins: [
+		[
+			'@luehrsenheinrich/postcss-wp-global-styles',
+			{
+				themeJson: './theme/theme.json',
+				colorPaletteGenerator,
+			},
+		],
+		['postcss-import'],
+		[
+			'postcss-preset-env',
+			{
+				stage: 1,
+				features: {
+					'custom-media-queries': {
+						preserve: false,
+						importFrom: './theme/src/css/vars/_media-queries.css',
+					},
+					'custom-properties': {
+						preserve: true,
+						importFrom: './theme/src/css/vars.css',
+					},
+				},
+			},
+		],
+		['cssnano'],
+	],
+};
