68c5c5662f5cef5c0dac0764ad89ea35b5155f16
Hendrik Luehrsen <hendrik@luehrsen.de>
2023-02-25 16:49:56 +0100

    Remove the settings view

--- theme/inc/Block_Patterns/bp-example.php
+++ theme/inc/Block_Patterns/bp-example.php
@@ -11,6 +11,6 @@
 <p>A simple example pattern. If you can read this at prod call an admin.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:image {"id":,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/block-patterns/placeholder_000.png" alt="" class=""/></figure>
+<!-- wp:image {"id":,"sizeSlug":"large","linkDestination":"none", "align":"wide"} -->
+<figure class="wp-block-image alignwide"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/block-patterns/placeholder_000.png" alt="" class=""/></figure>
 <!-- /wp:image -->
