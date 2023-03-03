9bf038864f5e5113ca8838389826b9fc65570fa0
Christopher Kanitz <ckanitz@users.noreply.github.com>
2023-03-03 13:43:58 +0100

    use globsync to create project archives (#164)

--- bin/archiveProject.js
+++ bin/archiveProject.js
@@ -4,63 +4,57 @@
 const fs = require('fs');
 const archiver = require('archiver');
 const ignore = require('ignore');
-const { glob } = require('glob');
+const { globSync } = require('glob');
 const pkg = require('./../package.json');
 
 const createArchive = (path, srcPath, slug) => {
-    // Define the output stream
-    const output = fs.createWriteStream(path + '/' + slug + '.zip');
+	// Define the output stream
+	const output = fs.createWriteStream(path + '/' + slug + '.zip');
 
-    // Initialize the archive
-    const archive = archiver('zip', {
-        zlib: { level: 9 } // Sets the compression level.
-    });
+	// Initialize the archive
+	const archive = archiver('zip', {
+		zlib: { level: 9 }, // Sets the compression level.
+	});
 
-    // Listen for all archive data to be written
-    archive.pipe(output);
+	// Listen for all archive data to be written
+	archive.pipe(output);
 
-    // Initialize the ignorer
-    const ig = ignore();
+	// Initialize the ignorer
+	const ig = ignore();
 
-    // We ignore files defined in a potential .distignore file
-    if( fs.existsSync(srcPath + '/.distignore') ) {
-        ig.add(fs.readFileSync(srcPath + '/.distignore', 'utf8').toString());
-    }
+	// We ignore files defined in a potential .distignore file
+	if (fs.existsSync(srcPath + '/.distignore')) {
+		ig.add(fs.readFileSync(srcPath + '/.distignore', 'utf8').toString());
+	}
 
-    glob('**/*',
-        {
-            cwd: srcPath,
-            nodir: true,
-        },
-        (err, files) => {
-            if( err ) {
-                console.error(err);
-                return;
-            }
+	try {
+		const files = globSync('**/*', {
+			cwd: srcPath,
+			nodir: true,
+		});
 
-            // Add all files to the archive
-            files.forEach(file => {
+		files.forEach((file) => {
+			// Skip ignored files
+			if (ig.ignores(file)) {
+				return;
+			}
 
-                // Skip ignored files
-                if( ig.ignores(file) ) {
-                    return;
-                }
+			// Construct the file path
+			const filePath = srcPath + '/' + file;
 
-                // Construct the file path
-                const filePath = srcPath + '/' + file;
+			// Add the file to the archive
+			archive.file(filePath, {
+				name: file,
+				prefix: slug,
+			});
+		});
 
-                // Add the file to the archive
-                archive.file(filePath, {
-                    name: file,
-                    prefix: slug,
-                });
-            });
+		// Finalize the archive
+		archive.finalize();
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-            // Finalize the archive
-            archive.finalize();
-        }
-    );
-}
-
-createArchive('./archives', './plugin', pkg.slug + 'p' );
-createArchive('./archives', './theme', pkg.slug + 't' );
+createArchive('./archives', './plugin', pkg.slug + 'p');
+createArchive('./archives', './theme', pkg.slug + 't');
