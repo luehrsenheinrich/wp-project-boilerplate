c65c1f0b6e897128ac60474e3680c449236dd6be
Christopher Kanitz <ckanitz@users.noreply.github.com>
2023-05-19 21:56:38 +0200

    rewrite IconSelectControl component to use `useIcons()` hook

--- plugin/admin/src/js/components/icon-select-control/index.js
+++ plugin/admin/src/js/components/icon-select-control/index.js
@@ -1,48 +1,40 @@
 /**
  * WordPress dependencies.
  */
-import { useSelect } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
+import {
+	BaseControl,
+	Spinner,
+	useBaseControlProps,
+} from '@wordpress/components';
 
 /**
  * External dependencies.
  */
 import Select from 'react-select';
+import { useIcons } from '../../data/entities/icon';
 
 /**
  * Internal dependencies.
  */
 import LHIcon from '../icon';
-import { ICONS_STORE_KEY } from '../../data';
 
 const IconSelectControl = ({
 	label = null,
 	value,
 	onChange,
-	allowedIcons = [],
 	blackList,
 	whiteList,
+	baseProps = {},
 }) => {
 	const [selectedOption, setSelectedOption] = useState();
-
-	const { icons } = useSelect((select) => {
-		const { getIcons } = select(ICONS_STORE_KEY);
-		let iconsFromSelect = getIcons();
-
-		if (allowedIcons.length) {
-			iconsFromSelect = [...iconsFromSelect].filter(
-				(icon) => allowedIcons.indexOf(icon.slug) > -1
-			);
-		}
-
-		return {
-			icons: iconsFromSelect || [],
-		};
-	});
+	const { baseControlProps, controlProps } = useBaseControlProps(baseProps);
+	const { icons, ...yadda } = useIcons();
 
 	useEffect(() => {
 		if (icons?.length && selectedOption?.value !== value) {
 			const icon = icons.find((i) => i?.slug === value) || {};
+			// Prepare the Select value.
 			setSelectedOption({
 				icon: { ...icon },
 				value: icon.slug,
@@ -53,47 +45,59 @@ const IconSelectControl = ({
 
 	const onSelectIcon = (option) => {
 		onChange(option?.value);
-
 		if (!option?.value) {
 			setSelectedOption(null);
 		}
 	};
 
-	let options = icons;
+	let options = icons?.slice() || []; // Create a copy of the icons array
+
 	// Filter options over black- or whitelist, prefering white over blacklist.
 	if (whiteList?.length) {
-		options = icons.filter((option) => whiteList.indexOf(option.slug) > -1);
+		options = options.filter(
+			(option) => whiteList.indexOf(option.slug) > -1
+		);
 	} else if (blackList?.length) {
-		options = icons.filter((option) => blackList.indexOf(option.slug) < 0);
+		options = options.filter(
+			(option) => blackList.indexOf(option.slug) < 0
+		);
 	}
+
 	return (
-		<>
+		<BaseControl {...baseControlProps}>
 			{label?.length && (
 				<label htmlFor="lh-icon-select-control">{label}</label>
 			)}
-			<Select
-				openMenuOnClick={true}
-				openMenuOnFocus={true}
-				classNamePrefix="react-select"
-				className="lh-icon-select-control react-select"
-				value={selectedOption}
-				onChange={onSelectIcon}
-				isSearchable={true}
-				isDisabled={!icons.length}
-				isClearable={true}
-				options={options.map((icon) => ({
-					icon: { ...icon },
-					value: icon.slug,
-					label: icon.title,
-				}))}
-				getOptionLabel={(option) => (
-					<div className="lh-icon-select-option-label">
-						<LHIcon slug={option.icon.slug} svg={option.icon.svg} />
-						<span>{option.label}</span>
-					</div>
-				)}
-			/>
-		</>
+			{!yadda?.hasResolved && <Spinner />}
+			{yadda?.hasResolved && (
+				<Select
+					{...controlProps}
+					openMenuOnClick={true}
+					openMenuOnFocus={true}
+					classNamePrefix="react-select"
+					className="lh-icon-select-control react-select"
+					value={selectedOption}
+					onChange={onSelectIcon}
+					isSearchable={true}
+					isDisabled={!icons.length}
+					isClearable={true}
+					options={options.map((icon) => ({
+						icon: { ...icon },
+						value: icon.slug,
+						label: icon.title,
+					}))}
+					getOptionLabel={(option) => (
+						<div className="lh-icon-select-option-label">
+							<LHIcon
+								slug={option.icon.slug}
+								svg={option.icon.svg}
+							/>
+							<span>{option.label}</span>
+						</div>
+					)}
+				/>
+			)}
+		</BaseControl>
 	);
 };
 
