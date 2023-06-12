b7aaecf664021ee5fa5071345bd09ec7ece910ff
Christopher Kanitz <ckanitz@users.noreply.github.com>
2023-06-12 15:00:33 +0200

    sort theme components alphabetically

--- theme/inc/Theme.php
+++ theme/inc/Theme.php
@@ -15,11 +15,32 @@ namespace WpMunich\lhpbpt;
 class Theme {
 
 	/**
+	 * Block patterns component.
+	 *
+	 * @var Block_Patterns\Block_Patterns
+	 */
+	protected $block_patterns;
+
+	/**
+	 * FSE component.
+	 *
+	 * @var FSE\FSE
+	 */
+	protected $fse;
+
+	/**
 	 * The i18n component.
 	 *
 	 * @var i18n\I18N
 	 */
 	protected $i18n;
+
+	/**
+	 * Lazysizes component.
+	 *
+	 * @var Lazysizes\Lazysizes
+	 */
+	protected $lazysizes;
 
 	/**
 	 * The Nav Menus component.
@@ -50,47 +71,26 @@ class Theme {
 	protected $theme_supports;
 
 	/**
-	 * FSE component.
-	 *
-	 * @var FSE\FSE
-	 */
-	protected $fse;
-
-	/**
-	 * Lazysizes component.
-	 *
-	 * @var Lazysizes\Lazysizes
-	 */
-	protected $lazysizes;
-
-	/**
-	 * Block patterns component.
-	 *
-	 * @var Block_Patterns\Block_Patterns
-	 */
-	protected $block_patterns;
-
-	/**
 	 * Constructor.
 	 *
+	 * @param Block_Patterns\Block_Patterns $block_patterns Block_Patterns component.
+	 * @param FSE\FSE                       $fse FSE component.
 	 * @param i18n\I18N                     $i18n I18N component.
+	 * @param Lazysizes\Lazysizes           $lazysizes Lazysizes component.
 	 * @param Nav_Menus\Nav_Menus           $nav_menus Nav_Menus component.
 	 * @param Scripts\Scripts               $scripts Scripts component.
 	 * @param Styles\Styles                 $styles Styles component.
 	 * @param Theme_Supports\Theme_Supports $theme_supports Theme_Supports component.
-	 * @param FSE\FSE                       $fse FSE component.
-	 * @param Lazysizes\Lazysizes           $lazysizes Lazysizes component.
-	 * @param Block_Patterns\Block_Patterns $block_patterns Block_Patterns component.
 	 */
 	public function __construct(
+		Block_Patterns\Block_Patterns $block_patterns,
+		FSE\FSE $fse,
 		i18n\I18N $i18n,
+		Lazysizes\Lazysizes $lazysizes,
 		Nav_Menus\Nav_Menus $nav_menus,
 		Scripts\Scripts $scripts,
 		Styles\Styles $styles,
-		Theme_Supports\Theme_Supports $theme_supports,
-		FSE\FSE $fse,
-		Lazysizes\Lazysizes $lazysizes,
-		Block_Patterns\Block_Patterns $block_patterns
+		Theme_Supports\Theme_Supports $theme_supports
 	) {
 		$this->i18n           = $i18n;
 		$this->nav_menus      = $nav_menus;
