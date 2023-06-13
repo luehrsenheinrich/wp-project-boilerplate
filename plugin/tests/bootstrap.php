71390134e36b61a015fa2f094a4e5919583fe691
Christopher Kanitz <ckanitz@users.noreply.github.com>
2023-06-13 16:13:27 +0200

    use WP_CONTENT_DIR in plugin test bootstrap as well

--- plugin/tests/bootstrap.php
+++ plugin/tests/bootstrap.php
@@ -32,7 +32,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/lhpbpp.php';
+	require WP_CONTENT_DIR . '/plugins/plugin/lhpbpp.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
