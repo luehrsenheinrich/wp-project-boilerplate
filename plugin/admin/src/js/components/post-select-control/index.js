d86e7c963ff332ad3cac64418d0ef02d949b1e7a
Christopher Kanitz <ckanitz@users.noreply.github.com>
2023-06-14 10:08:51 +0200

    Upd/data select controls/use wp data hooks (#257)
    * housekeeping
    
    * remove icon data store
    
    * register icon endpoints to entity controller, add hooks
    
    * rewrite the LHIcon component to use `useIcon()` hook
    
    * rewrite IconSelectControl component to use `useIcons()` hook
    
    * rewrite post select control component to use `useEntityRecords()` hook
    
    * properly wrap both controls with `<BaseControl>`
    
    * Update index.js
    
    * better option label check

--- plugin/admin/src/js/components/post-select-control/index.js
+++ plugin/admin/src/js/components/post-select-control/index.js
@@ -5,16 +5,19 @@
 /**
  * WordPress dependencies.
  */
-import { useEffect, useState } from '@wordpress/element';
-import apiFetch from '@wordpress/api-fetch';
-import { addQueryArgs } from '@wordpress/url';
+import { useMemo } from '@wordpress/element';
+import {
+	BaseControl,
+	Spinner,
+	useBaseControlProps,
+} from '@wordpress/components';
+import { useEntityRecords } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
-import { PanelRow } from '@wordpress/components';
 
 /**
  * External dependencies.
  */
-import AsyncSelect from 'react-select/async';
+import Select from 'react-select';
 import { DndContext } from '@dnd-kit/core';
 import { restrictToParentElement } from '@dnd-kit/modifiers';
 import {
@@ -29,28 +32,45 @@ import {
 	MultiValueContainer,
 } from './multi-value';
 
-const PostSelectControl = ({
+const EntitySelectControl = ({
+	kind: entityKind = 'postType',
+	name: entityName = 'post',
 	value,
 	onChange,
-	endpoint = 'posts',
 	query = {},
-	label = '',
+	label = __('Select posts', 'lhpbpp'),
 	help = '',
 	multiple = true,
-	max,
 }) => {
-	const [selectedOptions, setSelectedOptions] = useState([]);
+	const { baseControlProps, controlProps } = useBaseControlProps({
+		label,
+		help,
+	});
+	const { records: entityRecords = [], isResolving } = useEntityRecords(
+		entityKind,
+		entityName,
+		{
+			per_page: -1, // This actually returns ALL records, not just 100.
+			context: 'view',
+			...query,
+		}
+	);
 
-	if (typeof label === 'undefined') {
-		label = __('Select a post', 'jitmp');
-	}
+	const options = entityRecords
+		?.filter(({ name, title }) => {
+			// Don't show entities with empty titles.
+			const optionLabel = title?.rendered || title || name;
+			return typeof optionLabel === 'string' && optionLabel.length > 0;
+		})
+		?.map(({ id, name, title }) => ({
+			value: id,
+			label: title?.rendered || title || name,
+		}));
 
-	/**
-	 * Endpoint path.
-	 *
-	 * @type {string}
-	 */
-	const endpointPath = `/wp/v2/${endpoint}`;
+	const selectValue = useMemo(
+		() => getSelectValue(value, options, multiple),
+		[value, options, multiple]
+	);
 
 	/**
 	 * Handle a change in the selected option.
@@ -58,90 +78,7 @@ const PostSelectControl = ({
 	 * @param {*} option The selected option
 	 */
 	const onSelectPost = (option) => {
-		setSelectedOptions(option);
-
-		if (!multiple) {
-			onChange(option?.value);
-		} else {
-			onChange(option.map((o) => o.value));
-		}
-	};
-
-	let defaultSelectedOptionValues = [];
-
-	/**
-	 * Iterate over the value and compose an array of selected values.
-	 */
-	if (multiple) {
-		defaultSelectedOptionValues = value;
-	} else {
-		defaultSelectedOptionValues = value;
-	}
-
-	/**
-	 * A set of options that are available by default.
-	 *
-	 * @type {Array}
-	 */
-	const defaultOptions = [];
-
-	/**
-	 * Load the default options. Must include the options that are already
-	 * selected.
-	 */
-	const loadDefaultOptions = () => {
-		let newSelectedOptions = [];
-
-		// Check if selectedOptions is an array.
-		if (Array.isArray(selectedOptions)) {
-			newSelectedOptions = selectedOptions;
-		} else {
-			newSelectedOptions.push(selectedOptions);
-		}
-
-		return apiFetch({
-			path: addQueryArgs(endpointPath, {
-				per_page: 10,
-				include: defaultSelectedOptionValues,
-				...query,
-			}),
-		}).then((response) => {
-			/**
-			 * Iterate over the response and add the options to the default
-			 * options.
-			 */
-			const responseOptions = response.map((post) => {
-				const option = {
-					value: post.id,
-					// First: Post, Sec: search, Fallback: Taxonomy/Term.
-					label: post?.title?.rendered || post?.title || post.name,
-				};
-
-				/**
-				 * If this post is in the defaultSelectedOption, add it to the state.
-				 */
-				if (
-					multiple &&
-					defaultSelectedOptionValues?.includes(post.id)
-				) {
-					newSelectedOptions.push(option);
-				} else if (post.id === defaultSelectedOptionValues) {
-					newSelectedOptions.push(option);
-				}
-
-				defaultOptions.push(option);
-
-				return option;
-			});
-
-			/**
-			 * Update the state with the new options after we've completed
-			 * iterating over the response.
-			 */
-			setSelectedOptions(newSelectedOptions);
-
-			return responseOptions;
-		});
+		onChange(option || {});
 	};
 
 	const onSortEnd = (event) => {
@@ -149,120 +86,92 @@ const PostSelectControl = ({
 
 		if (!active || !over) return;
 
-		// const sortItems = (items) => {
-		// 	console.log({items});
-		// 	const oldIndex = items.findIndex(
-		// 		(item) => item.value === active.id
-		// 	);
-		// 	const newIndex = items.findIndex((item) => item.value === over.id);
-
-		// 	return arrayMove(items, oldIndex, newIndex);
-		// };
-
-		const oldIndex = selectedOptions.findIndex(
+		const oldIndex = selectValue.findIndex(
 			(item) => item.value === active.id
 		);
-		const newIndex = selectedOptions.findIndex(
+		const newIndex = selectValue.findIndex(
 			(item) => item.value === over.id
 		);
 
-		const sortedItems = arrayMove(selectedOptions, oldIndex, newIndex);
+		const sortedItems = arrayMove(selectValue, oldIndex, newIndex);
 
-		setSelectedOptions(sortedItems);
 		onChange(sortedItems);
 	};
 
-	useEffect(() => {
-		if (!selectedOptions?.length) {
-			loadDefaultOptions();
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [selectedOptions]);
-
-	/**
-	 * When the endpoint changes, we need to reset the selected options.
-	 */
-	useEffect(() => {
-		setSelectedOptions([]);
-		defaultOptions.length = 0;
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [endpoint]);
-
-	/**
-	 * Load the options for the select control from the API.
-	 *
-	 * @param {string} inputValue The search term.
-	 * @return {Promise} The options to display.
-	 */
-	const loadOptions = (inputValue = null) => {
-		return new Promise((resolve) => {
-			return apiFetch({
-				path: addQueryArgs(endpointPath, {
-					per_page: 10,
-					search: inputValue,
-					...query,
-				}),
-			}).then((response) => {
-				resolve(
-					response.map((post) => ({
-						value: post.id,
-						// First: Post, Sec: search, Fallback: Taxonomy/Term.
-						label:
-							post?.title?.rendered || post?.title || post.name,
-						post,
-					}))
-				);
-			});
-		});
-	};
-
 	return (
-		<PanelRow className="post-type-select__row">
-			<span className="post-type-select__label">{label}</span>
-			{multiple && (
-				<DndContext
-					modifiers={[restrictToParentElement]}
-					onDragEnd={onSortEnd}
-				>
-					<SortableContext
-						items={selectedOptions.map((o) => o.value)}
-						strategy={horizontalListSortingStrategy}
-					>
-						<AsyncSelect
-							value={selectedOptions}
+		<BaseControl {...baseControlProps}>
+			{isResolving && <Spinner />}
+			{!isResolving && (
+				<>
+					{multiple && (
+						<DndContext
+							modifiers={[restrictToParentElement]}
+							onDragEnd={onSortEnd}
+						>
+							<SortableContext
+								items={options?.map((o) => o.value) || []}
+								strategy={horizontalListSortingStrategy}
+							>
+								<Select
+									{...controlProps}
+									value={selectValue}
+									onChange={onSelectPost}
+									options={options}
+									getOptionLabel={(option) => option.label}
+									isClearable
+									isMulti
+									isSearchable={true}
+									openMenuOnClick={true}
+									openMenuOnFocus={true}
+									closeMenuOnSelect={false}
+									className={
+										('react-select',
+										'react-select-multi',
+										'react-select-sortable')
+									}
+									classNamePrefix={'react-select'}
+									components={{
+										MultiValue,
+										MultiValueContainer,
+										MultiValueRemove,
+									}}
+								/>
+							</SortableContext>
+						</DndContext>
+					)}
+					{!multiple && (
+						<Select
+							value={selectValue}
 							onChange={onSelectPost}
-							loadOptions={loadOptions}
-							defaultOptions={true}
+							options={options}
+							getOptionLabel={(option) => option.label}
+							isClearable
+							isSearchable={true}
+							openMenuOnClick={true}
+							openMenuOnFocus={true}
+							closeMenuOnSelect={false}
 							className={'react-select'}
 							classNamePrefix={'react-select'}
-							isClearable
-							isMulti
-							isOptionDisabled={() => max && value?.length >= max}
-							closeMenuOnSelect={false}
-							components={{
-								MultiValue,
-								MultiValueContainer,
-								MultiValueRemove,
-							}}
 						/>
-					</SortableContext>
-				</DndContext>
+					)}
+				</>
 			)}
-			{!multiple && (
-				<AsyncSelect
-					value={selectedOptions}
-					onChange={onSelectPost}
-					loadOptions={loadOptions}
-					defaultOptions={true}
-					className={'react-select'}
-					classNamePrefix={'react-select'}
-					isClearable
-					isOptionDisabled={() => max && value?.length >= max}
-				/>
-			)}
-			{help && <p className="post-type-select__help">{help}</p>}
-		</PanelRow>
+		</BaseControl>
 	);
 };
 
-export default PostSelectControl;
+export const PostSelectControl = (props) => {
+	return <EntitySelectControl {...props} name={'post'} />;
+};
+
+export default EntitySelectControl;
+
+function getSelectValue(value, options = [], multiple) {
+	if (multiple) {
+		return value?.map((v) => {
+			return options.find((o) => o.value === v.value);
+		});
+	}
+
+	return options.find((o) => o.value === value?.value);
+}
