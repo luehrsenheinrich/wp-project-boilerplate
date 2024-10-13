46c69b4e0bce58c7eac06dd3b33c49cfa305bc3e
Hendrik Luehrsen <hendrik@luehrsen.de>
2024-10-13 17:18:41 +0200

    Disable the customization of theme.json

--- bin/updateThemeJsonSchema.js
+++ bin/updateThemeJsonSchema.js
@@ -13,27 +13,6 @@ request.get('https://schemas.wp.org/trunk/theme.json', (err, res, body) => {
 		 */
 		const themeJsonSchema = JSON.parse(body);
 
-		/**
-		 * Modify the default theme schema to our needs.
-		 */
-		const palette =
-			themeJsonSchema.definitions.settingsPropertiesColor.properties.color
-				.properties.palette;
-
-		palette.items.properties = {
-			...palette.items.properties,
-			hover: {
-				description:
-					'CSS hex or rgba(a) string of a matching hover color.',
-				type: 'string',
-			},
-			contrast: {
-				description:
-					'CSS hex or rgba(a) string of a matching contrast color.',
-				type: 'string',
-			},
-		};
-
 		const fileContent = JSON.stringify(themeJsonSchema, null, 2);
 
 		fs.writeFile('./schemas/theme.json', fileContent, (error) => {
