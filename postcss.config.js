4b0e3c8725284cba503b320683c0afc28ab5b273
Hendrik Luehrsen <hendrik@luehrsen.de>
2026-03-18 16:34:13 +0100

    Improve lint configs and async PostCSS setup
    Add ESLint import resolver and an import/no-unresolved rule to ignore `^@wordpress/` imports; update composer and npm scripts to run PHP linters via `php` with adjusted error_reporting to suppress deprecation noise and to use vendor binaries. Prefix the JS lint command with BROWSERSLIST_IGNORE_OLD_DATA to avoid browserslist warnings. Convert postcss.config.js to an async function that dynamically imports @csstools/postcss-global-data and registers postcss-preset-env and cssnano plugins for better ESM compatibility and plugin initialization.

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,27 @@
-module.exports = {
-	plugins: [
-		['postcss-import'],
-		[
-			'@csstools/postcss-global-data',
-			{
-				files: [
-					'./theme/src/css/vars.css',
-					'./theme/src/css/vars/_media-queries.css',
-				],
-			},
+module.exports = async function () {
+	const { default: postcssGlobalData } = await import(
+		'@csstools/postcss-global-data'
+	);
+
+	return {
+		plugins: [
+			['postcss-import'],
+			[
+				postcssGlobalData,
+				{
+					files: [
+						'./theme/src/css/vars.css',
+						'./theme/src/css/vars/_media-queries.css',
+					],
+				},
+			],
+			[
+				'postcss-preset-env',
+				{
+					stage: 1,
+				},
+			],
+			['cssnano'],
 		],
-		[
-			'postcss-preset-env',
-			{
-				stage: 1,
-			},
-		],
-		['cssnano'],
-	],
+	};
 };
